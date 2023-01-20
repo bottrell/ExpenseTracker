88a900bde022449da7a624c93c726fe2a8147882
Jordan Bottrell <bottrell@umich.edu>
2023-01-20 08:38:53 -0500

    refactored column to listview

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -6,51 +6,55 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-  
+
   TransactionList({required this.transactions, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-            child: Row(
-          children: [
-            Container(
-                padding: const EdgeInsets.all(10),
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                child: Text(
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+    return Container(
+      height: 500,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                  padding: const EdgeInsets.all(10),
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(
                       color: Colors.purple,
+                      width: 2,
                     ),
-                    "\$${tx.cost.toString()}")),
-            Column(
-              //
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(tx.title!,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 15)),
-                Text(
-                  DateFormat.yMMMMd().format(tx.date!),
-                  style: const TextStyle(
-                    color: Colors.grey,
                   ),
-                ),
-              ],
-            )
-          ],
-        ));
-      }).toList(),
+                  child: Text(
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                      "\$${transactions[index].cost.toString()}")),
+              Column(
+                //
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(transactions[index].title!,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15)),
+                  Text(
+                    DateFormat.yMMMMd().format(transactions[index].date!),
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ));
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
